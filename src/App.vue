4fdc3fbfc02393a1112c874c68030abbea5747b1
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-07 12:48:40 +0100

    Added custom query on results

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script>
 import SearchBar from "./components/SearchBar.vue";
+import axios from "axios";
 
 export default {
+  data() {
+    return {
+      films: [],
+    };
+  },
+
   components: {
     SearchBar,
   },
 
   methods: {
     fetchResults(query) {
-      console.log(query);
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "9662f4631b63862b49fb1bf5e49bd400",
+            query: query,
+          },
+        })
+        .then((results) => {
+          this.films = results.data.results;
+          console.log(this.films);
+        });
     },
   },
 };
